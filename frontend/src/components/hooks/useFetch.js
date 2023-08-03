0afa70c02c42b27a92ed8b43e4366aed5291ee69
SaurabhKarki <saurabh.karki98176@gmail.com>
2023-08-03 14:14:33 +0545

    Made the first customhook.

---
+++ frontend/src/components/hooks/useFetch.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async () => {
+      try {
+        const { data } = await axios(url);
+
+        if (data) {
+          setLoading(false);
+          setData(data);
+        }
+      } catch (error) {
+        setLoading(false);
+        setError(error);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+  return { data, loading, error };
+};
+
+export default useFetch;
