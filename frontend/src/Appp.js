4ab39040e2bcbb01487d5dedb864f68e660682cd
SaurabhKarki <saurabh.karki98176@gmail.com>
2023-08-04 14:59:04 +0545

    Brought back the changes made earlier.

--- frontend/src/Appp.js
+++ frontend/src/Appp.js
@@ -1,31 +1,14 @@
-import { Container, Row, Col } from "react-bootstrap";
-
 import Title from "./components/Title";
-import Todo from "./pages/Todo";
-import AddTodo from "./components/AddTodo";
 
-import { API_SERVER } from "./constants";
-
-// import CustomHook from "./components/CustomHook";
+import CustomHook from "./components/CustomHook";
 
 function Appp() {
   return (
     <>
-      <Container>
-        <Row>
-          <Col md={{ span: 6, offset: 3 }}>
-            <Title name="TO DO app" />
-            <AddTodo
-              label="Add New Task"
-              placeholder="Eg: Do Clean the house"
-              url={`${API_SERVER}/todos`}
-            />
-            <Todo />
-          </Col>
-        </Row>
-      </Container>
+      <Title name="TO DO app" />
+
+      <CustomHook />
     </>
   );
 }
-
 export default Appp;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,12 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Appp from "./Appp";
 
-import DataContextProvider from "./contexts";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <DataContextProvider>
-      <Appp />
-    </DataContextProvider>
+    <Appp />
   </React.StrictMode>
 );
